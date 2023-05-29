6c0cad6d7547371610e050eeeb787517d7e5e20d
mashkamashka <romanova98ma@mail.ru>
2023-05-29 14:33:37 +0300

    home work

---
+++ Hello_world_Console/Homework/Program.cs
@@ -0,0 +1,23 @@
+﻿int count = 0;
+int friend = 2;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int time = 1;
+
+while (distance >10)
+{
+    if (friend == 1)
+    {
+     time = distance/(firstFriendSpeed+dogSpeed);  
+     friend = 2;
+    }
+    
+    time = distance/(secondFriendSpeed+dogSpeed);
+    friend = 1;
+
+distance = distance-(firstFriendSpeed+secondFriendSpeed)*time;
+count++;
+}
+ Console.WriteLine(count);
