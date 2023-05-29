3aac436965f9f17c00fb932a2801b0b547801e8d
mashkamashka <romanova98ma@mail.ru>
2023-05-29 17:37:33 +0300

    Дни недели

---
+++ Hello_world_Console/Sem1_Task003/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.Write ("Понедельник");
+}
+ 
+if (number == 2) 
+{
+    Console.Write ("Вторник");
+}
+
+if (number == 3)
+{
+    Console.Write ("Среда");
+}
+
+if (number == 4)
+{
+    Console.Write ("Четверг");
+}
+
+if (number == 5)
+{
+    Console.Write ("Пятница");
+}
+
+if (number == 6)
+{
+    Console.Write ("Суббота");
+} 
+
+if (number == 7)
+{
+    Console.Write ("Воскресенье");
+}
+
+if (number>7 || number<1)
+{
+    Console.Write ("Введен неверный номер дня недели!");
+}
