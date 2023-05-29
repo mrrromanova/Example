da475a416e5e481528fe68b0cdf22b22d01e0204
mashkamashka <romanova98ma@mail.ru>
2023-05-29 17:14:19 +0300

    Извлечение корня

---
+++ Hello_world_Console/Sem1_Ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число a: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberB == Convert.ToInt32(Math.Sqrt(numberA)))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
