ad61d8fe2c907c543532c2b80483613494675467
mashkamashka <romanova98ma@mail.ru>
2023-05-29 18:14:44 +0300

    Последняя цифра

---
+++ Hello_world_Console/Sem1_Task05/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число:");
+int number = int.Parse(Console.ReadLine());
+int B = number%10;
+Console.WriteLine($"Последняя цифра этого числа: {B}");
